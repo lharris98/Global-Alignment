935ec451b77a9b176b1bce248169860715b9c7b4
lharris98 <74216509+lharris98@users.noreply.github.com>
2020-12-07 16:57:20 -0600

    Update global_alignment.py

--- global_alignment.py
+++ global_alignment.py
@@ -11,7 +11,7 @@ parser.add_argument(
 
 args= parser.parse_args()
 
-records = [record for record in SeqIO.parse(args.filenmae,'fasta')]
+records = [record for record in SeqIO.parse(args.filename,'fasta')]
 
 if len(records) != 2:
     print("Too many sequences")
@@ -21,7 +21,6 @@ if len(records) != 2:
 
 seq_1,seq_2 = records
 
-# Import format_alignment method
 from Bio.pairwise2 import format_alignment
 
 # A match score of 2, a mismatch score of -1.Gap open penalty of -0.5 and gap extension penalty of -0.1
